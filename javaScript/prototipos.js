f92211b43f8594455d173d29571e4049f1d8a289
OneAimlessRobot <adri.campos.valente@gmail.com>
2023-05-18 18:32:36 +0100

    THis new commit

---
+++ javaScript/prototipos.js
@@ -0,0 +1,15 @@
+function Car(make, model, year,motor){
+
+	this.make= make;
+	this.model=model;
+	this.year=year;
+	this.motor="b8 motor";
+
+}
+
+Car.prototype.toString = function(){
+
+	return "
+
+}
+/*Todas as funcoes possuem um objeto vazio caso sejam usadas como construtores.*/
---
+++ javaScript/tests.js
@@ -0,0 +1,12 @@
+
+
+function main(){
+	let m= {nome :"champ",loc: "here"};
+
+	console.log(m.nome +" is "+m.loc);
+	
+	return 0
+
+}
+
+main()
