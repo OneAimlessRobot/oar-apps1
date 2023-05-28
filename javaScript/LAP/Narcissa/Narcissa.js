82f7488a3bfae4ce8e600473966bfa334421fe06
OneAimlessRobot <adri.campos.valente@gmail.com>
2023-05-28 23:17:59 +0100

    Hopefully I finish this

--- javaScript/LAP/Narcissa/Narcissa.js
+++ javaScript/LAP/Narcissa/Narcissa.js
@@ -26,13 +26,14 @@ const IMAGE_NAME_INVALID = "invalid";
 const IMAGE_NAME_SHRUB = "shrub";
 const IMAGE_NAME_BERRY_BLUE = "berryBlue";
 const IMAGE_NAME_SNAKE_HEAD = "snakeHead";
+const IMAGE_NAME_SNAKE_MODULE = "snakeHead";
 
 
 // GLOBAL VARIABLES
 
 let control;	// Try not no define more global variables
-
-
+let pause=0;
+let time=0
 // ACTORS
 
 class Actor {
@@ -61,35 +62,44 @@ class Actor {
 		this.y += dy;
 		this.show();
 	}
+	handleObjects(){
+	}
 	animation(x, y) {
 	}
 	checkPosition() {
-		if (control.world[this.x][this.y] === undefined){
-			if(this.x>=69){
+			if(this.x>=70){
 			
-				this.x=2
+				this.x=1
 			}
-			else if(this.x<=1){
+			if(this.x<=0){
 			
-				this.x=68
+				this.x=69
 			}
-			else if(this.y>=39){
+			if(this.y>=40){
 			
-				this.y=2
+				this.y=1
 			}
-			else if(this.y<=1){
+			if(this.y<=0){
 			
-				this.y=38
+				this.y=39
 			}
-		
-		
-		} 
+		 
 	}
 }
 
 
 class Shrub extends Actor {
-	constructor(x, y, color) { super(x, y, IMAGE_NAME_SHRUB); }
+	constructor(x, y, color) { 
+	super(x, y, IMAGE_NAME_SHRUB); 
+	this.components=[]
+	
+	
+	}
+	grow(){
+	
+	
+	
+	}
 }
 
 class Empty extends Actor {
@@ -112,10 +122,21 @@ class Berry extends Actor {
 	}
 }
 
+
+class SnakeModule extends Actor {
+	constructor(x,y) {
+		super(x,y, IMAGE_NAME_SNAKE_HEAD);
+	}
+}
+
 class Snake extends Actor {
 	constructor(x, y) {
 		super(x, y, IMAGE_NAME_SNAKE_HEAD);
 		[this.movex, this.movey] = [1,0];
+		this.eatables=[IMAGE_NAME_BERRY_BLUE,IMAGE_NAME_SHRUB]
+		this.objectsAround=[]
+		this.lastPositions=[]
+		this.trailLength=0
 	}
 	handleKey() {
 		let k = control.getKey();
@@ -133,9 +154,91 @@ class Snake extends Actor {
 			//mesg("change direction == " + k)
 		}
 	}
+	getPos(){
+	
+		return [this.x,this.y]
+	}
+	setPos(newX,newY){
+		[this.x,this.y]=[newX,newY]
+		
+	}
+	handleObjects(){
+		if(this.objectsAround.find(e => e.imageName === 'shrub')){
+			this.die();
+		}
+		if(this.objectsAround.find(e => e.imageName === "berryBlue")){
+			let k=0;
+			for(;k<this.objectsAround.length;k++){
+				let j=this.objectsAround[k]
+				if(j.imageName==="berryBlue"){
+				
+					j.hide()
+					this.eat();
+				}	
+				
+			
+			}
+		}
+	
+	}
+	renderTrail(){
+		for(let i=0;i<this.trailLength;i++){
+			control.world[this.lastPositions[i][0]][this.lastPositions[i][1]]=new SnakeModule(this.lastPositions[i][0],this.lastPositions[i][1])
+			control.world[this.lastPositions[i][0]][this.lastPositions[i][1]].show()
+		}
+		
+		
+	
+	}
+	hideTrail(){
+		for(let i=0;i<this.lastPositions.length;i++){
+			control.world[this.lastPositions[i][0]][this.lastPositions[i][1]].hide()
+		}
+		
+		
+	
+	}
+	eat(){
+		this.trailLength+=1
+		}
 	animation(x, y) {
 		this.handleKey();
+		if(this.lastPositions.length>0){
+		this.hideTrail();
+		}
 		this.move(this.movex, this.movey);
+		this.eyes()
+		this.lastPositions.push([this.x-this.movex,this.y-this.movey])
+		if(this.lastPositions.length>this.trailLength){
+		this.lastPositions.shift();
+		}
+		if(this.lastPositions.length>0){
+		this.renderTrail();
+		}
+	}
+	eyes(){
+		this.objectsAround=[
+		control.world[this.x-1][this.y-1],//1
+		control.world[this.x+1][this.y+1],//2
+		control.world[this.x-1][this.y],//3
+		control.world[this.x][this.y+1],//4
+		control.world[this.x+1][this.y],//5
+		control.world[this.x][this.y-1],//6
+		control.world[this.x-1][this.y+1],//7
+		control.world[this.x+1][this.y-1]//8
+		]
+		
+		/*     168 	
+		       3S5	
+		       742	
+		*/
+		
+		
+	}
+	die(){
+	
+		this.hide()
+		this.hideTrail()
 	}
 }
 
@@ -185,7 +288,8 @@ class GameControl {
 			case 37: case 79: case 74: return [-1, 0];	// LEFT, O, J
 			case 38: case 81: case 73: return [0, -1];	// UP, Q, I
 			case 39: case 80: case 76: return [1, 0];	// RIGHT, P, L
-			case 40: case 65: case 75: return [0, 1];	// DOWN, A, K
+			case 40: case 65: case 75: return [0,1];	// DOWN, A, K
+			case 32: togglePause()
 			case 0: return null;
 			default: return String.fromCharCode(k);
 		// http://www.cambiaresearch.com/articles/15/javascript-char-codes-key-codes
@@ -198,12 +302,16 @@ class GameControl {
 	}
 	animationEvent() {
 		this.time++;
+		time++;
 		for(let x=0 ; x < WORLD_WIDTH ; x++)
 			for(let y=0 ; y < WORLD_HEIGHT ; y++) {
 				let a = this.world[x][y];
 				if( a.atime < this.time ) {
+					if(pause==0){
 					a.atime = this.time;
 					a.animation(x, y);
+					a.handleObjects();
+				}
 				}
 			}
 	}
@@ -216,10 +324,24 @@ class GameControl {
 
 
 // Functions called from the HTML page
-
+function togglePause(){
+	if(pause==1){
+	
+		pause=0
+	}
+	else{
+	
+		pause=1;
+	}	
+	
+}
 function onLoad() {
 	// Asynchronously load the images an then run the game
 	GameImages.loadAll(() => new GameControl());
+}
+function getTime(){
+
+	return time;
 }
 
 function b1() { mesg("button1") }
