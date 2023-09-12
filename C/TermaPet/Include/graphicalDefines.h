a31a16a21e6854da39c86ab724c60dea9cf723ba
OneAimlessRobot <adri.campos.valente@gmail.com>
2023-09-13 00:27:04 +0100

    hhhhhhh

--- C/TermaPet/Include/graphicalDefines.h
+++ C/TermaPet/Include/graphicalDefines.h
@@ -6,12 +6,12 @@
 #define HUDY 0
 #define TITLEH 3
 #define TITLEW 44
-#define STATSW ((int)(STATBARSIZES)+51)
 #define STATSH 15
+#define STATSW ((int)(STATBARSIZES)+51)
 #define STATSX (HUDX+1)
 #define STATSY HUDY
 #define MENUH 30
-#define MENUW 20
+#define MENUW 40
 #define MENUX HUDX
 #define MENUY HUDY
 #define SUBMENUH 0
@@ -22,7 +22,7 @@
 #define PETW 11
 #define PETX SUBMENUX
 #define PETY (HUDY+3)
-#define WARNINGSX HUDX
+#define WARNINGSX STATSX
 #define WARNINGSY 17
 #define GOHOMEH 9
 #define GOHOMEW 10
