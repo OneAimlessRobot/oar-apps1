219610b0cec5e3c47f17feb0a9ff34bd57ebadf3
OneAimlessRobot <adri.campos.valente@gmail.com>
2023-10-29 10:44:28 +0000

    adddskkakddskak

--- C/rampplayer/editor/main.c
+++ C/rampplayer/editor/main.c
@@ -4,7 +4,7 @@
 
 
 
-int main(int argc, char ** argv){
+int editor(int argc, char ** argv){
 
                 FILE* file;
 char* stringBuff=malloc(1024);
--- C/rampplayer/main.c
+++ C/rampplayer/main.c
@@ -1,25 +1,22 @@
 #include "Includes/preprocessor.h"
+#include "editor/Includes/editor.h"
 
 int main(int argc, char ** argv){
 	
-	char buff[1024],buff2[1024],buff3[1024],buff4[1024];
+	char buff[1024],buff2[1024];
 	memset(buff ,0 ,1024);
 	memset(buff2 ,0 ,1024);
-	char* tmpfilepathsfile=" $(echo $(pwd))/filepaths ";
+	char* tmpfilepathsfile="./filepaths";
 	strcpy(buff,"find ");
-	strcpy(buff4,"rm ");
+	strcpy(buff2,"rm ");
 	strcat(buff,argv[1]);
 	strcat(buff," -maxdepth 1 -type f > ");
 	strcat(buff,tmpfilepathsfile);
 	system(buff);
-	strcpy(buff2," $(echo $(pwd))/editor/editor ");
-	strcpy(buff3,argv[2]);
+	argv[1]=tmpfilepathsfile;
+	editor(argc,argv);
 	strcat(buff2,tmpfilepathsfile);
-	strcat(buff2,buff3);
 	system(buff2);
-	strcat(buff4,tmpfilepathsfile);
-	system(buff4);
-	
 
 	return 0;
 }
